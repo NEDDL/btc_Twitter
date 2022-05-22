0f57bc134c4ad8d57495f0ae0cd0690e2eef13ba
Mustafa ONAL <mustafasaitonal@outlook.com>
2022-05-22 18:03:33 +0200

    schema fix

--- src/database/models/tweetModel.ts
+++ src/database/models/tweetModel.ts
@@ -4,12 +4,12 @@ import Tweet from "../interfaces/tweet";
 const TweetSchema: Schema = new Schema(
   {
     _id: { type: String, required: true },
-    dateTimePub: { type: String, required: true },
+    dateTimePub: { type: String, required: false },
     sentiment: { type: Number, required: false },
     url: { type: String, required: true },
     title: { type: String, required: true },
-    image: { type: String, required: true },
-    relevance: { type: Number, required: true },
+    image: { type: String, required: false },
+    relevance: { type: Number, required: false },
     isPublished: { type: Boolean, required: true },
     twittedAt: { type: Date, required: false }
   },
